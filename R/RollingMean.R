55e2859f058bb4a1ca1aed4f1798ce070cbd84c5
ebikei <ebikei@hotmail.com>
2014-12-09 20:54:46 -0500

    Update RollingMean.R

--- R/RollingMean.R
+++ R/RollingMean.R
@@ -12,6 +12,7 @@ output_location=c('/home/bst/other/kebisu/BW_PMCoarse/Result/')
 
 load('AirData_20141206.Rdata') #AirData
 PMCoarse_Data=filter(AirData,!is.na(PMCoarse))
+PMCoarse_Data$FIPS_County[PMCoarse_Data$FIPS_County=='12086']='12025'
 rm(AirData)
 
 #Select Monitors which have more than 1 yr observation period and 120 observations.
@@ -36,29 +37,31 @@ setkey(PMCoarse_Data2,FIPS_County,Date.Local)
 PMCoarse_Data3=PMCoarse_Data2[CJ(unique(FIPS_County),seq(min(Date.Local),max(Date.Local),by=1))]
 PMCoarse_Data3[,PMCoarse_Week:=rollapply(PMCoarse,7,mean,align=c("right"),fill=NA,na.rm=TRUE),by=FIPS_County]
 PMCoarse_Data3$PMCoarse_Week[is.nan(PMCoarse_Data3$PMCoarse_Week)]=NA
-PMCoarse_Data3$CO<-PMCoarse_Data3$NO2<-PMCoarse_Data3$SO2<-PMCoarse_Data3$PM10<-PMCoarse_Data3$PM25<-PMCoarse_Data3$n<-PMCoarse_Data3$PMCoarse<-NULL
+PMCoarse_Data3$CO<-PMCoarse_Data3$NO2<-PMCoarse_Data3$SO2<-PMCoarse_Data3$PM10<-PMCoarse_Data3$PM25<-PMCoarse_Data3$n<-NULL
 rm(list=setdiff(ls(), c('PMCoarse_Data3')))
 
 BirthList=data.table(read.csv('BirthOutcomeFips_Gest_Combination.csv'))
 BirthList$FIPS_County=sprintf("%05d",BirthList$res_fips)
+BirthList$LMPDate2=as.Date(paste(substr(BirthList$LMPDate,1,4),substr(BirthList$LMPDate,5,6),substr(BirthList$LMPDate,7,8),sep='-'),"%Y-%m-%d")
 BirthList$dgestat=NULL
 BirthList$res_fips=NULL
-setkey(BirthList,FIPS_County)
+setkey(BirthList,FIPS_County,LMPDate2)
+BirthList1=subset(unique(BirthList))
 
 CountyList=PMCoarse_Data3[!duplicated(PMCoarse_Data3$FIPS_County),list(FIPS_County)]
-BirthList2=merge(BirthList,CountyList,by='FIPS_County',all.y=TRUE)
-rm(BirthList)
-
-BirthList2$temp=as.Date(paste(substr(BirthList2$LMPDate,1,4),substr(BirthList2$LMPDate,5,6),substr(BirthList2$LMPDate,7,8),sep='-'),"%Y-%m-%d")
+BirthList2=merge(BirthList1,CountyList,by='FIPS_County',all.y=TRUE)
+rm(BirthList,BirthList1)
 
 DataMatrix=matrix(0,nrow=dim(BirthList2)[1],ncol=50)
 
 #i=1
 for (i in 1:50){
-BirthList3=mutate(BirthList2,temp2=temp+i*7-1)
-setkey(BirthList3,FIPS_County,temp2)
+BirthList3=mutate(BirthList2,Date.Local=LMPDate2+i*7-1)
+setkey(BirthList3,FIPS_County,Date.Local)
 test=PMCoarse_Data3[BirthList3]
+#df_mg=merge(PMCoarse_Data3,BirthList2,by=c('FIPS_County','Date.Local'),all.y=TRUE)
 DataMatrix[,i]=test$PMCoarse_Week
+#DataMatrix[,i]=df_mg$PMCoarse_Week
 rm(BirthList3,test)
 }
 
@@ -67,9 +70,12 @@ names=paste(rep('Week',50),1:50,sep='')
 colnames(BirthList4)[4:53]=names
 
 for (i in 30:50){
-BirthList4[[paste0('GestWeek_',i)]]=rowMeans(BirthList4[,c(4:i)],na.rm=TRUE)
+BirthList4[[paste0('GestWeek_',i)]]=rowMeans(BirthList4[,c(4:(3+i))],na.rm=TRUE)
 }
 
+BirthList4$First_ObsWks=apply(BirthList4,1,function(f) sum(!is.na(f[4:16])))
+BirthList4$Second_ObsWks=apply(BirthList4,1,function(f) sum(!is.na(f[17:29])))
 
+BirthList5=filter(BirthList4,First_ObsWks>9,Second_ObsWks>9)
 
 rm(list=ls())
